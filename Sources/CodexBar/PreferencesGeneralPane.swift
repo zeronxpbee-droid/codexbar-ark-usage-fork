1583d6cc10058b9aeb24a0d300cb6d3ce517e65d
Yuxin Qiao <104957188+Yuxin-Qiao@users.noreply.github.com>
2026-06-07 18:15:51 +0800

    feat: add native Dutch localization support

--- Sources/CodexBar/PreferencesGeneralPane.swift
+++ Sources/CodexBar/PreferencesGeneralPane.swift
@@ -12,6 +12,7 @@ enum AppLanguage: String, CaseIterable, Identifiable {
     case portugueseBrazilian = "pt-BR"
     case swedish = "sv"
     case french = "fr"
+    case dutch = "nl"
     case ukrainian = "uk"
 
     var id: String {
@@ -29,6 +30,7 @@ enum AppLanguage: String, CaseIterable, Identifiable {
         case .portugueseBrazilian: L("language_portuguese_brazilian")
         case .swedish: L("language_swedish")
         case .french: L("language_french")
+        case .dutch: L("language_dutch")
         case .ukrainian: L("language_ukrainian")
         }
     }
